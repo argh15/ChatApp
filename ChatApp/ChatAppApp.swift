adac4b0f65123d816eddc02f3fa370ef01336ba4
Arghadeep Chakraborty <argho.chakr@gmail.com>
2024-09-14 14:24:11 -0400

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Arghadeep Chakraborty on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
